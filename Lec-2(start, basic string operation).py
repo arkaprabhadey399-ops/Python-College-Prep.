cf66a883e12fcd95357890929727bfc54a0f7c0e
Arka-dev <arkaprabhadey399@gmail.com>
2026-06-01 21:26:01 +0530

    Add files via upload

---
+++ Lec-2(start, basic string operation).py
@@ -0,0 +1,92 @@
+"""===Basic String Operation==="""
+
+"""str1='This is a string'
+str2="This is a string"
+print(type(str1))
+print(type(str2))
+str3="This is a string"
+print(str1)
+print(str2)
+print(str3)
+"""
+
+
+"""===Concatation==="""
+
+"""str1="hello"+"World"
+str2="hello"+" "+"World"
+str3="Apna"
+str4="College"
+add=str3+str4
+add1=str3+" "+str4
+print(str1)
+print(str2)
+print(str3+str4)
+print(str3+" "+str4)
+print(add)
+print(add1)"""
+
+
+"""===Length of a String==="""
+
+"""var="Apna College"
+length=len(var)
+print(len(var))
+print(length)"""
+
+
+"""===ESCAPE SEQUENCE==="""
+
+"""str4="This is a string.\nwe are running it in python"
+str5="This is a string.\tWe are running it in python"
+print(str4)
+print(str5)"""
+
+
+"""===INDEXING==="""
+
+"""str="Apna College"
+I1=str[2]
+I2=str[4]
+print(str[0])
+print(str[6])
+print(I1)
+print(I2)"""
+
+
+"""===Slicing==="""
+
+"""str="Apna College"
+s1=str[1:5]
+s2=str[0:3]
+s3=str[4:5] 
+s4=str[1:6]                  
+entire1=(str[0:12])
+entire2=str[0:]
+entire3=str[0:len(str)]
+entire4=str[0:44]
+print(str[2:5])
+print(s1)
+print(s2)
+print(s3)
+print(s4)
+print(entire1)
+print(entire2)
+print(entire3)
+print(entire4)"""
+
+
+"""===Negative Index Slicing"""
+
+"""str="Apple"
+s1=str[-3:-1]
+s2=str[-5:-1]
+s3=str[-5:]
+s4=str[-5:len(str)]
+s5=str[:]
+print(s1)
+print(s2)
+print(s3)
+print(s4)
+print(s5)"""
+     
