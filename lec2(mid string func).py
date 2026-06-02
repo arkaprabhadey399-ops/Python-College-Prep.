525b025031a5469f75002461228475bca32b027f
Arka-dev <arkaprabhadey399@gmail.com>
2026-06-02 18:44:55 +0530

    Add files via upload

---
+++ lec2(mid string func).py
@@ -0,0 +1,32 @@
+"""===String Function==="""
+
+"""str="i am learning python from apna college"
+print(str.endswith("ge"))
+print(str.endswith("a"))
+print(str.capitalize())
+str=str.capitalize()
+print(str)
+print(str.replace("am","is"))
+print(str.replace("a","o"))
+print(str.find("l"))
+print(str.find("python"))
+print(str.count("am"))
+print(str.count("o"))"""
+
+
+"""=====QUESTION PEACTICE====="""
+
+"""===(Q1)==="""
+
+"""print("=====LENGTH OF NAME FINDER====")
+name=input("Enter Your First Name: " )
+print("Length of your first name is:",len(name))
+print("Your first name is",len(name),"characters long")"""
+
+
+"""===(Q2)==="""
+
+"""print("=====OCCURRENCE OF $ IN A STRING====")
+str="Hello I am $. I am one of the strongest currency in the world. $ is the most widely used currency. $ to ₹ rate is 95. $ is used in U.S.A "
+print(str.count("$"))
+print("Occurrence of $ in your string is", str.count("$"))"""
